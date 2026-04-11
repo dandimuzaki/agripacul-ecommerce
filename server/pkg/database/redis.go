04c201994ad31b196f07743beb540f78442e6b5e
dandimuzaki <dandimuzaki@gmail.com>
2026-04-11 21:53:54 +0700

    modify redis setting

--- server/pkg/database/redis.go
+++ server/pkg/database/redis.go
@@ -3,17 +3,16 @@ package database
 import (
 	"context"
 	"debian-ecommerce/pkg/utils"
-	"fmt"
 
 	"github.com/redis/go-redis/v9"
 )
 
 func InitRedis(config utils.RedisConfig) (*redis.Client, error) {
-	rdb := redis.NewClient(&redis.Options{
-		Addr:     fmt.Sprintf("%s:%s", config.Host, config.Port),
-		Password: config.Password,
-		DB:       config.DB,
-	})
+	opt, err := redis.ParseURL(config.Conn)
+	if err != nil {
+		return nil, err
+	}
+	rdb := redis.NewClient(opt)
 
 	if err := rdb.Ping(context.Background()).Err(); err != nil {
 		return nil, err
