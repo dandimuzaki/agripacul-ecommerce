38e343512767d6566a7761e1394494a2f3d7e05c
dandimuzaki <dandimuzaki@gmail.com>
2026-04-21 21:49:01 +0700

    refactor: improve payment method entity

--- server/pkg/database/redis.go
+++ server/pkg/database/redis.go
@@ -3,23 +3,22 @@ package database
 import (
 	"context"
 	"debian-ecommerce/pkg/utils"
-	"fmt"
 
 	"github.com/redis/go-redis/v9"
 )
 
 func InitRedis(config utils.RedisConfig) (*redis.Client, error) {
-	rdb := redis.NewClient(&redis.Options{
-		Addr:     fmt.Sprintf("%s:%s", config.Host, config.Port),
-		Password: config.Password,
-		DB:       config.DB,
-	})
+	// rdb := redis.NewClient(&redis.Options{
+	// 	Addr:     fmt.Sprintf("%s:%s", config.Host, config.Port),
+	// 	Password: config.Password,
+	// 	DB:       config.DB,
+	// })
 
-	// opt, err := redis.ParseURL(config.Conn)
-	// if err != nil {
-	// 	return nil, err
-	// }
-	// rdb := redis.NewClient(opt)
+	opt, err := redis.ParseURL(config.Conn)
+	if err != nil {
+		return nil, err
+	}
+	rdb := redis.NewClient(opt)
 
 	if err := rdb.Ping(context.Background()).Err(); err != nil {
 		return nil, err
