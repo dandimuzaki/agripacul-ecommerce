21cb8f552caeec5b743c666ff0ba11b43eccd6e2
dandimuzaki <dandimuzaki@gmail.com>
2026-04-21 23:00:29 +0700

    refactor: update payment method list component

--- server/internal/data/entity/payment.go
+++ server/internal/data/entity/payment.go
@@ -1,16 +1,26 @@
 package entity
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type PaymentType struct {
-	gorm.Model
+	ID        uint           `gorm:"primaryKey" json:"id"`
+  CreatedAt time.Time `json:"created_at"`
+  UpdatedAt time.Time `json:"updated_at"`
+  DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at"`
 	Name string `json:"name"`
 
 	Methods []PaymentMethod `gorm:"foreignKey:PaymentTypeID" json:"methods"`
 }
 
 type PaymentMethod struct {
-	gorm.Model
+	ID        uint           `gorm:"primaryKey" json:"id"`
+  CreatedAt time.Time `json:"created_at"`
+  UpdatedAt time.Time `json:"updated_at"`
+  DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at"`
 	PaymentTypeID uint `json:"payment_type_id"`
 	Name     string `gorm:"uniqueIndex;not null" json:"name"`
 	IsActive bool   `gorm:"default:true" json:"is_active"`
